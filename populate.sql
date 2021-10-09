4e5edffd2b78634c3c2b18f7d716ec38489a8fe6
Richard <brandtriku@gmail.com>
2021-10-09 21:07:00 +0300

    Added new table roles

--- populate.sql
+++ populate.sql
@@ -1,5 +1,8 @@
 --Needs to have atleast 1 account created
 
+
+
+
 INSERT INTO boards (id, name, moderator) VALUES (1, 'Random', 1);
 INSERT INTO boards (id, name, moderator) VALUES (2, 'Stoopid', 1);
 INSERT INTO boards (id, name, moderator) VALUES (3, 'SQL', 1);
@@ -12,9 +15,9 @@ INSERT INTO posts (post_owner, title, created_at, board) VALUES (1, 'Is that ran
 INSERT INTO posts (post_owner, title, created_at, board) VALUES (1, 'Is this random?', NOW(), 3);
 
 
-INSERT INTO messages (post_id, user_id, content, sent_at) VALUES (1, 1, 'My first post', NOW());
-INSERT INTO messages (post_id, user_id, content, sent_at) VALUES (2, 1, 'My sec post', NOW());
-INSERT INTO messages (post_id, user_id, content, sent_at) VALUES (3, 1, 'My 34d post', NOW());
-INSERT INTO messages (post_id, user_id, content, sent_at) VALUES (3, 1, 'My 3123 post', NOW());
-INSERT INTO messages (post_id, user_id, content, sent_at) VALUES (4, 1, 'My firasdst post', NOW());
-INSERT INTO messages (post_id, user_id, content, sent_at) VALUES (4, 1, 'My dasd post', NOW());
+INSERT INTO messages (post_id, user_id, content, created_at) VALUES (1, 1, 'My first post', NOW());
+INSERT INTO messages (post_id, user_id, content, created_at) VALUES (2, 1, 'My sec post', NOW());
+INSERT INTO messages (post_id, user_id, content, created_at) VALUES (3, 1, 'My 34d post', NOW());
+INSERT INTO messages (post_id, user_id, content, created_at) VALUES (3, 1, 'My 3123 post', NOW());
+INSERT INTO messages (post_id, user_id, content, created_at) VALUES (4, 1, 'My firasdst post', NOW());
+INSERT INTO messages (post_id, user_id, content, created_at) VALUES (4, 1, 'My dasd post', NOW());
--- schema.sql
+++ schema.sql
@@ -1,20 +1,27 @@
+DROP TABLE IF EXISTS roles CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS room CASCADE; 
 DROP TABLE IF EXISTS posts CASCADE;
 DROP TABLE IF EXISTS messages CASCADE;
+DROP TABLE IF EXISTS boards CASCADE;
+
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    role_name TEXT
+);
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR NOT NULL UNIQUE,
     password VARCHAR NOT NULL,
-    role integer NOT NULL DEFAULT '1'
+    role_id integer REFERENCES roles(id)
 );
 
 CREATE TABLE boards (
     id integer PRIMARY KEY,
     name TEXT,
-    moderator integer REFERENCES users(id)
-
+    moderator integer REFERENCES users(id),
+    visible boolean default true
 );
 
 CREATE TABLE posts (
@@ -22,12 +29,18 @@ CREATE TABLE posts (
     post_owner integer REFERENCES users(id),
     title TEXT,
     created_at TIMESTAMP,
-    board integer REFERENCES boards(id)
+    board integer REFERENCES boards(id),
+    visible boolean default true
 );
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     post_id integer REFERENCES posts(id),
     user_id integer REFERENCES users(id), 
     content TEXT,
-    sent_at TIMESTAMP
+    created_at TIMESTAMP,
+    visible boolean default true
 );
+
+INSERT INTO roles(role_name) VALUES ('USER');
+INSERT INTO roles(role_name) VALUES ('MODERATOR');
+INSERT INTO roles(role_name) VALUES ('ADMIN');
